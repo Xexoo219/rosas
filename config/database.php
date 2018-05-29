dc3a11b03d43b89fd2ece906752da2ccf5973547
sergio <sergio.ayala.del.castillo@gmail.com>
2018-05-29 10:39:39 -0400

    todo

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,11 +56,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'ec2-54-83-59-120.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'd1aq9j0mah9856'),
+            'username' => env('DB_USERNAME', 'zfkjjxxwpyoqdf'),
+            'password' => env('DB_PASSWORD', '683834888a3e032ead2bd9d851c05ede6cc2bc0da0364fd00e1391589b1da095'),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
