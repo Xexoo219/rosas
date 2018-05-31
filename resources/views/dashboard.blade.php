5f2706f1df5a60bdbc2fcfa98d19ac858ef1f45a
sergio <sergio.ayala.del.castillo@gmail.com>
2018-05-31 16:13:51 -0400

    arreglo dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -127,7 +127,7 @@
             <div class="icon">
              <i class="fa fa-users"></i>
             </div>
-            <a href="" class="small-box-footer"> <i class="fa fa-arrow-circle-right"></i></a>
+            <a href="" class="small-box-footer"></a>
           </div>
         </div>
 
