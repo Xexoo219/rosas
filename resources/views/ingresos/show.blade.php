570e52ec65edb3ebd89cf6abf19ffb497819c77f
sergio <sergio.ayala.del.castillo@gmail.com>
2018-06-06 15:22:58 -0400

    correcion

--- resources/views/ingresos/show.blade.php
+++ resources/views/ingresos/show.blade.php
@@ -44,7 +44,7 @@
        <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
         <h3>        <strong>Comentario:</strong>
-                {{ $ingresos->comentario}}</h3>
+                {{ $ingreso->created_at}}</h3>
             </div>
     </div>
 
