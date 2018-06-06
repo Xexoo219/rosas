b18c633894a1a52d9c445c8ace19151af53594de
sergio <sergio.ayala.del.castillo@gmail.com>
2018-06-06 17:12:44 -0400

    correcion

--- resources/views/salidas/show.blade.php
+++ resources/views/salidas/show.blade.php
@@ -49,7 +49,7 @@
     <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Ingresado:</strong>
-                {{$created = $salida->created_at->diffForHumans()}}
+                {{$created = $salida->created_at}}
            
             </div>
         </div>
