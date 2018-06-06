a6660c39a5734a293c05bd4eb525b1ca4349ff49
sergio <sergio.ayala.del.castillo@gmail.com>
2018-06-06 09:30:37 -0400

    todo

--- app/Curso.php
+++ app/Curso.php
@@ -5,9 +5,9 @@ namespace App;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
-use APP\Curso;
-use APP\Ensenanza;
-use APP\Estudiante;
+use App\Curso;
+use App\Ensenanza;
+use App\Estudiante;
 use DB;
 
 class Curso extends Model
