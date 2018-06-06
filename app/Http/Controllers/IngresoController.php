ba9d20c9d5689c8f5483c6b9cbc920f8706d6737
sergio <sergio.ayala.del.castillo@gmail.com>
2018-06-06 13:36:38 -0400

    correcion

--- app/Http/Controllers/IngresoController.php
+++ app/Http/Controllers/IngresoController.php
@@ -90,6 +90,7 @@ class IngresoController extends Controller
         $ensenanza = Ensenanza::all();
         $permisos_entrada = Permiso::all();
         $estudiante = Estudiante::findOrFail($id);
+        $ingreso = Ingreso::findOrFail($id);
         return view('ingresos.show',compact('ingreso','curso','ensenanza','estudiante'));
       
     }
