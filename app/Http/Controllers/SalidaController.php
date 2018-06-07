bb6d6c2c4bf843caebbceab142d51839fb55f161
sergio <sergio.ayala.del.castillo@gmail.com>
2018-06-07 08:00:37 -0400

    correcion

--- app/Http/Controllers/SalidaController.php
+++ app/Http/Controllers/SalidaController.php
@@ -93,7 +93,7 @@ class SalidaController extends Controller
      */
     public function show($id)
     {
-        $salida = Salida::findOrFail($id);
+
         $curso = Curso::all();
         $ensenanza = Ensenanza::all();
         $permisos_entrada = Permiso::all();
