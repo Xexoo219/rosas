cc2b0c4aa631999c2a5ae53c373d772cc079d337
sergio <sergio.ayala.del.castillo@gmail.com>
2018-06-12 16:06:43 -0400

    agregar restriciones dashboard

--- resources/views/permisos.blade.php
+++ resources/views/permisos.blade.php
@@ -1,16 +1,73 @@
 @extends('admin.layout')
 @section('content')
+
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+
+
+
     <section class="content-header">
                     @if (session('status'))
                         <div class="alert alert-success">
                             {{ session('status') }}
                         </div>
                     @endif
-
+<body>
       <h1>
         SIN PERMISOS. COMUNICARSE CON EL ADMINISTRADOR.
       
       </h1>
+      </body>
     </section>
   <section class="content">
 
